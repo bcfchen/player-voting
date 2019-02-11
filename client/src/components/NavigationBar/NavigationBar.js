b09b99980e0acc233770585a5c3d45133bfe04da
beck chen <beckchen@becks-MacBook-Pro.local>
2019-02-11 16:24:33 +0900

    [Beck] styling fixes to the nav bar button, to be continued...

--- client/src/components/NavigationBar/NavigationBar.js
+++ client/src/components/NavigationBar/NavigationBar.js
@@ -8,8 +8,8 @@ const NavigationBar = ({ onSwitchUser, isAdminMode, onEndVoting }) => {
                 <i className="fa fa-caret-down"></i>
             </button>
             <div className="dropdown-content">
-                <a href="#" onClick={onSwitchUser}>Switch User</a>
-                {isAdminMode && <a href="#" onClick={onEndVoting}>Toggle End Voting</a>}
+                <a href="#" onClick={onSwitchUser}><span>Switch User</span></a>
+                {isAdminMode && <a href="#" onClick={onEndVoting}><span>Toggle End Voting</span></a>}
             </div>
         </div>
     </div>);
